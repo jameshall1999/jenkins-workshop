60fb25096d1d6f5ef18aad2c1a3e25cb0e4d8d61
Will Gibson <will.gibson@walterego.co.uk>
2020-08-26 16:56:07 +0100

    Try to get the app to listen on the Heroku assigned port

--- api.py
+++ api.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
+port = int(os.environ.get("PORT", 5000))
 
 @app.route('/')
 def hello():
-    return "Hello World, it's wonderful top see you!"
+    return "Hello World, it's wonderful to see you!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
